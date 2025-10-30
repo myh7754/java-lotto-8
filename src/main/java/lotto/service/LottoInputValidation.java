8387777e03d9c96b3ff6ce7a0f208614318ec320
myh <myh4755@gmail.com>
2025-10-31 00:51:10 +0900

    feat(validation): 중복된 숫자 검증

--- src/main/java/lotto/service/LottoInputValidation.java
+++ src/main/java/lotto/service/LottoInputValidation.java
@@ -1,11 +1,19 @@
 package lotto.service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LottoInputValidation {
     public void validateSize(List<String> lottoNumbers) {
         if (lottoNumbers.size() != 6) {
             throw new IllegalArgumentException(String.format("[ERROR] 로또 번호 개수를 잘못입력하였습니다 : %d개 입력됨", lottoNumbers.size()));
+        }
+    }
+    public void validateDuplicate(List<String> lottoNumbers) {
+        Set<String> uniqueLottoNumbers = new HashSet<>(lottoNumbers);
+        if  (uniqueLottoNumbers.size() != lottoNumbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있습니다.");
         }
     }
 }
