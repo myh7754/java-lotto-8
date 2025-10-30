95f97c7c5e2130b47df4b65acd0edf45b396fa71
myh <myh4755@gmail.com>
2025-10-31 05:02:50 +0900

    feat(service): 입력한 금액만큼 계산하여 로또 발행 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,7 +1,9 @@
 package lotto.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.Lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoService {
@@ -11,5 +13,13 @@ public class LottoService {
 
     public List<Integer> generateLottoNumbers() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+
+    public List<Lotto> generateLottos(int lottoCount) {
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            lottos.add(new Lotto(generateLottoNumbers()));
+        }
+        return lottos;
     }
 }
