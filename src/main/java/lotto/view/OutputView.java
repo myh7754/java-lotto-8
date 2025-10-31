263836e2471203aa1badb50868e492d9e34bf67c
myh <myh4755@gmail.com>
2025-10-31 17:43:44 +0900

    feat(view): 로또 번호 출력 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,18 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+
+import java.util.List;
+
 public class OutputView {
     public void printNumberOfPurchases(int purchaseAmount) {
         System.out.println(purchaseAmount+"개를 구매했습니다.");
+    }
+
+    public void printWinningNumbers(List<Lotto> lottos) {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
     }
 
 }
